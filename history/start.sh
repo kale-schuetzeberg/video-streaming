91a34ccdb9daf42604d85e4484cbf4b04c4af1da
Justin 'Kale' Schuetzeberg <justin.schuetzeberg@gmail.com>
2024-02-18 20:22:21 -0600

    implement indirect-messaging with RabbitMQ, prod working

--- history/start.sh
+++
@@ -1,6 +0,0 @@
-# Start spring boot devtools process in the background.
-# This is necessary for hot reload.
-./gradlew -t :bootJar &
-
-# Next, start the app.
-./gradlew bootRun
--- video-streaming/src/main/java/com/baddog/videostreaming/VideoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.baddog.videostreaming;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-public interface VideoRepo extends MongoRepository<VideoDO, String> {
-  @Query("{id: '?0'}")
-  VideoDO getVideoById(String id);
-}
