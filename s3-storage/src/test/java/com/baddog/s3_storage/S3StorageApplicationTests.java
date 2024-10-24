e3a37c73f2fa74850b7fa82265f22c9291af774b
Kale Schuetzeberg <kaleschuetzeberg@gmail.com>
2024-10-23 19:31:36 -0500

    initialize microservice project

---
+++ s3-storage/src/test/java/com/baddog/s3_storage/S3StorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baddog.s3_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3StorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
